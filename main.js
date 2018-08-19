c2b6eaf0dae5c7e61256c8332e7b50f8c9a69948
Shencs <34359927+shencss@users.noreply.github.com>
2018-08-20 05:01:43 +0800

    修改BLOGS样式，实现CONTACT部分的动画
    1.更换箭头icon
    2.给Blog项添加下划线
    3.在进入skill-list和mail-box-bg底部时执行相应的特效
    4.判断是否滚动到页面底部，有避免重复无效代码的执行

--- main.js
+++ main.js
@@ -38,12 +38,12 @@ const contact = document.querySelector('.contact');
 
 
 const navHeight = nav.offsetHeight;
-let scrollHeightNow =  document.body.scrollTop || document.documentElement.scrollTop; 
+let scrollHeightNow =  document.documentElement.scrollTop || document.body.scrollTop; 
 
 
 //如果滚动超过导航条高度则导航条缩小
 function handleOnNavSilde() {
-    let scrollHeightNext =  document.body.scrollTop || document.documentElement.scrollTop; 
+    let scrollHeightNext =  document.documentElement.scrollTop || document.body.scrollTop; 
     //showNav布尔值是为了减少DOM操作
     let showNav = true;
     //如果滚动高度超过Nav高度
@@ -66,42 +66,60 @@ function handleOnNavSilde() {
 } 
 
 //判断元素顶部是否进入可视区域
-function isElementInViewport(el) {
+function isElementTopInViewport(el) {
     var rect = el.getBoundingClientRect();
-    return rect.top < (window.innerHight || document.documentElement.clientHeight);
-    
+    return rect.top < (window.innerHight || document.documentElement.clientHeight); 
+}
+//判断元素底部是否进入可视区域
+function isElementBottomInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.bottom < (window.innerHight || document.documentElement.clientHeight); 
 }
 
 //如果元素进入顶部进入可是区域则通过fade-in类形成fadeIn特效
 function addCSSFadeIn(el) {
-    if(isElementInViewport(el)) {
+    if(isElementTopInViewport(el)) {
         el.classList.add('fade-in');
     }
 }
 //为introduction及所有block执行addCSSFadeIn函数
 let hasReachBottom = false;
+//页面高度
+const pageHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+//浏览器高度
+const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+const blocks = document.getElementsByClassName('block');
+const introduction = document.querySelector('.introduction');
+const skillList = document.querySelector('.skill-list');
+const percentages =  document.getElementsByClassName('skill-percentage');
+const mailBoxBg = document.querySelector('.mail-box-bg');
+const mailBox = document.querySelector('.mail-box');
+const emailAddress = document.querySelector('.email-address');
+const githubAddress = document.querySelector('.github-address');
 function handleOnElementFadeIn() {
     //滚到到底部之后便结束
     if(hasReachBottom) return;
-
-    const blocks = document.getElementsByClassName('block');
-    const introduction = document.querySelector('.introduction');
-    //len - 1：contact部分不需要fadein特效
+    //单独为introduction添加fadein特效
+    addCSSFadeIn(introduction); 
+    //len - 1是因为contact部分不需要fadein特效
     for(let i = 0, len = blocks.length; i < len - 1; i++) {
         addCSSFadeIn(blocks[i]);
-
-        //滚动到SKILLSET部分时将percentage的display出来，而它的伪类会执行动画
-        if(i == 2) {
-            const percentages =  document.getElementsByClassName('skill-percentage');
-            for(let j = 0, len = percentages.length; j < len; j++) {
-                percentages[j].style.display = 'block';
-            }
-        }
     }
-    addCSSFadeIn(introduction);    
-
-    //一旦滚动到过底部
-    if(!hasReachBottom && isElementInViewport(contact)) {
+    //滚动到CONTACT的mail-box-bg底部时将mail-box呈现出来，而它及它的伪类会执行动画
+    //同时为两个address添加动画
+    if(isElementBottomInViewport(mailBoxBg)) {
+        mailBox.style.display = 'block';
+        emailAddress.style.animation = 'address-in .5s 1s ease-in-out both';
+        githubAddress.style.animation = 'address-in .5s 1s ease-in-out both';
+    } 
+    //滚动到SKILLSET的skill-list底部时将percentage呈现出来，而它的伪类会执行动画
+    if(isElementBottomInViewport(skillList)) {
+        for(let j = 0, len = percentages.length; j < len; j++) {
+            percentages[j].style.display = 'block';
+        }
+    }  
+    //scrollHeightNow + clientHeight = pageHeight时即滚动到底部，不再执行重复无效的代码
+    if(!hasReachBottom && (scrollHeightNow + clientHeight == pageHeight)) {
         hasReachBottom = true;
     }  
 }
@@ -119,6 +137,7 @@ for(let i = 0, len = detailBtns.length; i < len; i++) {
     detailBtns[i].addEventListener('click', () => {
         cover.style.display = 'block';
         noteDetails[i].style.display = 'block';
+        //animation使得note-detail渐变进入
         noteDetails[i].style.animation = 'detail-fade-in .5s linear both';
         //点击遮罩或关闭按钮则关闭
         cover.addEventListener('click', () => {
